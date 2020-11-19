45e24b78fd48210c73f75b5609d9f238ccd43365
Caroline Ham <cyham@umich.edu>
2020-11-19 16:13:05 -0500

    Added unit test for addBcc(String... emails) function (takes multiple emails)

--- src/test/java/org/apache/commons/mail/EmailTest.java
+++ src/test/java/org/apache/commons/mail/EmailTest.java
@@ -1,7 +1,10 @@
 package org.apache.commons.mail;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 public class EmailTest {
 	// Test Emails
@@ -20,4 +23,13 @@ public class EmailTest {
 	public void tearDownEmailTest() throws Exception {
 		// Do nothing on tear down
 	} // END of the tear down function
+	
+	// Test for addBcc(String... emails) function
+	@Test
+	public void testAddBcc() throws Exception {
+		// Call addBcc with TEST_EMAILS
+		email.addBcc(TEST_EMAILS);
+		// Check if the number of BCC'd emails is 5
+		assertEquals(5, email.getBccAddresses().size());
+	} // END of Test for addBcc(String... emails)
 }
